5741b70ce708fd6d586bcb27b9d9e2e547fbdbac
eduardolagoeiro <eduardolagoeiro@id.uff.br>
2019-03-17 21:57:38 -0300

    refactor server and app

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
+const app = require('./app');
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
